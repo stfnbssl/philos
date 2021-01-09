9157e04b24c45de08b303a2a9a5f9d0c214ff0b2
stfnbssl <stfn.bssl@gmail.com>
2021-01-09 14:19:26 +0100

    dev 09 01 21

--- packages/wizzi.plugin.philos/dist/lib/wizzi/models/philos-factory.g.js
+++ packages/wizzi.plugin.philos/dist/lib/wizzi/models/philos-factory.g.js
@@ -1,7 +1,7 @@
 /*
     artifact generator: C:\My\wizzi\stfnbssl\philos\node_modules\wizzi-legacy-v5\lib\artifacts\js\module\gen\main.js
     primary source IttfDocument: c:\my\wizzi\stfnbssl\philos\node_modules\wizzi-core\lib\artifacts\wfschema\factory\gen\ittf\wfschema-factory.js.ittf
-    utc time: Fri, 08 Jan 2021 11:40:09 GMT
+    utc time: Sat, 09 Jan 2021 07:09:46 GMT
 */
 'use strict';
 /**
